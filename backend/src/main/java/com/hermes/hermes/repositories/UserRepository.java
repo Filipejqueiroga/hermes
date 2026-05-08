5dcf9021beb5fab0d14f24e57b94265ef63dfbc5
Filipe Queiroga <filipejqueiroga@gmail.com>
2026-05-08 10:23:58 -0300

    feat(auth): add UserRepository with findByEmail and findByVerificationCode

--- backend/src/main/java/com/hermes/hermes/repositories/UserRepository.java
+++ backend/src/main/java/com/hermes/hermes/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.hermes.hermes.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.hermes.hermes.entities.User;
@@ -7,4 +9,6 @@ import com.hermes.hermes.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationCode(String verificationCode);
 }
